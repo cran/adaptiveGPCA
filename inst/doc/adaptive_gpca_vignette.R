86cdb66e5190428a20a5fce23a8e1d12da692027
Julia Fukuyama <julia.fukuyama@gmail.com>
2022-12-08 10:42:31 +0000

    version 0.1.3

--- inst/doc/adaptive_gpca_vignette.R
+++ inst/doc/adaptive_gpca_vignette.R
@@ -1,34 +1,34 @@
-## ---- echo = FALSE-------------------------------------------------------
+## ---- echo = FALSE------------------------------------------------------------
 library(knitr)
 opts_chunk$set(fig.width = 6, fig.height = 4)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(adaptiveGPCA)
 library(ggplot2)
 library(phyloseq)
 data(AntibioticPhyloseq)
 theme_set(theme_bw())
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 pp = processPhyloseq(AntibioticPhyloseq)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 out.agpca = adaptivegpca(pp$X, pp$Q, k = 2)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 out.agpca
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 plot(out.agpca)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 plot(out.agpca, type = "samples", axes = c(1,2))
 plot(out.agpca, type = "variables", axes = c(1,2))
 
-## ----eval = FALSE--------------------------------------------------------
+## ----eval = FALSE-------------------------------------------------------------
 #  inspectTaxonomy(out.agpca, AntibioticPhyloseq)
 
-## ---- eval = FALSE-------------------------------------------------------
+## ---- eval = FALSE------------------------------------------------------------
 #  out.ff = gpcaFullFamily(pp$X, pp$Q, k = 2)
 #  out.agpca = visualizeFullFamily(out.ff,
 #                      sample_data = sample_data(AntibioticPhyloseq),
@@ -36,12 +36,12 @@ plot(out.agpca, type = "variables", axes = c(1,2))
 #                      var_data = tax_table(AntibioticPhyloseq),
 #                      var_mapping = aes(x = Axis1, y = Axis2, color = Phylum))
 
-## ----fig.width=7---------------------------------------------------------
+## ----fig.width=7--------------------------------------------------------------
 ggplot(data.frame(out.agpca$U, sample_data(AntibioticPhyloseq))) +
     geom_point(aes(x = Axis1, y = Axis2, color = type, shape = ind)) +
     xlab("Axis 1") + ylab("Axis 2")
 
-## ----fig.width=9---------------------------------------------------------
+## ----fig.width=9--------------------------------------------------------------
 ggplot(data.frame(out.agpca$QV, tax_table(AntibioticPhyloseq))) +
     geom_point(aes(x = Axis1, y = Axis2, color = Phylum)) +
     xlab("Axis 1") + ylab("Axis 2")
